89a421418a167e6527af9d1679c5490bbc17fd75
Juneid Teklu <te.juneid@gmail.com>
2024-10-28 10:03:00 -0500

    Initial express app setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import "./styles.css";
-/*  */
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Express server is active at port ${PORT}!`));
