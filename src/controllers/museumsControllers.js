cb58115dc7e20283b7979a3e30030d22e96deb2b
Juneid Teklu <te.juneid@gmail.com>
2024-11-01 14:38:57 -0500

    Create validation chain for museum form

--- src/controllers/museumsControllers.js
+++ src/controllers/museumsControllers.js
@@ -1,4 +1,32 @@
 const queries = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const validationMiddleware = [
+  body("museumName")
+    .trim()
+    .notEmpty()
+    .withMessage("You must enter the name of the museum")
+    .matches(/^[A-Za-z0-9 .,'\-]+$/)
+    .withMessage("* No special characters")
+    .isLength({ min: 1, max: 60 })
+    .withMessage("* You must enter a museum name"),
+  body("city")
+    .trim()
+    .notEmpty()
+    .withMessage("You must enter the city the museum is located in")
+    .matches(/^[A-Za-z0-9 .,'\-]+$/)
+    .withMessage("* No special characters")
+    .isLength({ min: 1, max: 40 })
+    .withMessage("* You must enter a city"),
+  body("country")
+    .trim()
+    .notEmpty()
+    .withMessage("You must enter the country the museum is located in")
+    .matches(/^[A-Za-z0-9 .,'\-]+$/)
+    .withMessage("* No special characters")
+    .isLength({ min: 1, max: 40 })
+    .withMessage("* You must enter a country"),
+];
 
 async function allMuseumsGet(req, res) {
   const rows = await queries.getAllMuseums();
@@ -21,8 +49,12 @@ async function museumInfoGet(req, res) {
     museumArtworks: museumArtworks,
   });
 }
+async function newMuseumPost(req, res) {}
+
 module.exports = {
   allMuseumsGet,
   newMuseumFormGet,
   museumInfoGet,
+  newMuseumPost,
+  validationMiddleware,
 };
